d7ebdd9d22e299770cf1a188a5d1a23677a93813
Waqar Hussain <115903012+waqarabaaj@users.noreply.github.com>
2024-01-06 15:56:22 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "i.pinimg.com",
+      "plus.unsplash.com",
+      "indoreinstitute.com",
+      "images.theconversation.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
